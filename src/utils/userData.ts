c1fef3d3aee826ed6a083a628b5ad0f95d4fd7bb
PzLeMoNBR <marciogabrieloficial@hotmail.com>
2023-05-15 11:57:26 -0300

    feat : complete project

---
+++ src/utils/userData.ts
@@ -0,0 +1,8 @@
+export const userData = {
+  nameUser: "Marcio Gabriel",
+  githubUser: "MarcioCalenzo",
+  linkedinUser: "marciocalenzo",
+  whatsappNumber: "+5521980713406",
+  emailUser: "marciogabrieloficial@hotmail.com",
+  instagramUser: "marciobiel_cl",
+};
