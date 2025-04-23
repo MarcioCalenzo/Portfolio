33f9cf5e6880fad9b32d871661951b8599099185
Marcio Calenzo <106695254+MarcioCalenzo@users.noreply.github.com>
2025-04-23 17:48:55 -0300

    Update userData.ts

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -2,7 +2,7 @@ export const userData = {
   nameUser: "Marcio Gabriel",
   githubUser: "MarcioCalenzo",
   linkedinUser: "marciocalenzo",
-  whatsappNumber: "+5521980713406",
+  whatsappNumber: "+5521966858471",
   emailUser: "marciogabrieloficial@hotmail.com",
   instagramUser: "marciobiel_cl",
 };
